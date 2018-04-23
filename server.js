ddca87f883063321d52e051127258f18f8f54ebb
Christian <christian_hane@aol.com>
2018-04-22 18:04:55 -0700

    stuff

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const router = require('./routes/router.js');
 
 const PORT = process.env.PORT || 3001;
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static('./client/build'));
 
